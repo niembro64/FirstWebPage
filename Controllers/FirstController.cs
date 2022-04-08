692c08aef9023bb8874fddbc26719ccde30465b9
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-07 23:09:49 -0400

    simple routing

--- Controllers/FirstController.cs
+++ Controllers/FirstController.cs
@@ -4,7 +4,31 @@ namespace FirstWebPage.Controllers
 {
   public class FirstController : Controller
   {
-      
+    // what type of route?
+
+    // get
+    [HttpGet]
+    // tell it what name of route is
+    [Route("")] // empty is front page
+    public ViewResult Index()
+    {
+      // return "Hello from the controller - edited version";
+      return View("Index");
+    }
+
+
+        // get
+    [HttpGet]
+    // tell it what name of route is
+    [Route("/second")] // empty is front page
+    public ViewResult Second()
+    {
+      // return "Hello from the controller - edited version";
+      return View("Second");
+    }
+
+    // post
+
 
   }
 
